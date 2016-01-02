31af6859aad9570ff4f23b0a3440d605aaf89cb6
root-sudip <touch.das@gmail.com>
2016-01-02 20:24:25 +0530

    linux

---
+++ dir/special.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+//S_IFIFO
+//S_IFCHR
+//S_IFDIR
+//S_IFBLK
+//S_IFREG
+//Permission
+//S_IRUSR
+//S_IWUSR
+//S_IXUSR
+//S_IRWXU
+//S_IRWXG
+//S_IRWXO
+
+int main(){
+	int return_val;
+
+	return_val = mknod ("char", S_IRUSR| S_IWUSR|S_IFIFO, 0);	
+	if(return_val < 0){
+		printf("Error to create file \n");
+	}
+	else{
+		printf("Successfully created \n");
+	}
+
+
+	return 0;
+}
